99242f7378ab39833034426f04d2bc8eed4a4977
conchonha <thaibasang8@gmail.com>
2022-04-06 20:20:30 +0700

    Android project library

---
+++ AndroidLibrary/src/main/java/com/sangtb/androidlibrary/OperationSangTb.kt
@@ -0,0 +1,9 @@
+package com.sangtb.androidlibrary;
+
+/*
+    Copyright © 2022 UITS CO.,LTD
+    Created by SangTB on 4/6/2022
+*/
+public class OperationSangTb {
+    fun Tong(a : Int,b : Int) = a+b
+}
