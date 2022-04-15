17b5a1229c9088f72bb91e61e430ff1a4e8ec258
conchonha <thaibasang8@gmail.com>
2022-04-15 23:49:04 +0700

    update library sangtb v0.1.0

--- AndroidLibrary/src/main/java/com/sangtb/androidlibrary/utils/SingleLiveEvent.kt
+++ AndroidLibrary/src/main/java/com/sangtb/androidlibrary/utils/SingleLiveEvent.kt
@@ -1,41 +1,55 @@
-package com.sangtb.androidlibrary.utils
-
-import androidx.lifecycle.*
+package com.sangtb.game.utils;
 
 /*
     Copyright © 2022 UITS CO.,LTD
-    Created by SangTB on 4/6/2022
+    Created by SangTB on 4/15/2022
 */
 
+import android.util.Log
+import androidx.annotation.MainThread
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.atomic.AtomicBoolean
+/**
+ * A lifecycle-aware observable that sends only new updates after subscription, used for events like
+ * navigation, Toast and Snackbar messages.
+ *
+ *
+ * This avoids a common problem with events: on configuration change (like rotation) an update
+ * can be emitted if the observer is active. This LiveData only calls the observable if there's an
+ * explicit call to setValue() or call().
+ *
+ *
+ * Note that only one observer is going to be notified of changes.
+ */
 class SingleLiveEvent<T> : MutableLiveData<T>() {
-
-    private val liveDataToObserve: LiveData<T>
-    private val pendingMap: MutableMap<Int, Boolean>
-
-    init {
-        val outputLiveData = MediatorLiveData<T>()
-        outputLiveData.addSource(this) { currentValue ->
-            outputLiveData.value = currentValue
-        }
-        liveDataToObserve = outputLiveData
-        pendingMap = HashMap()
-    }
-
+    private val pending = AtomicBoolean(false)
+    @MainThread
     override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
-
-        pendingMap[observer.hashCode()] = false
-
+        if (hasActiveObservers()) {
+            Log.w(TAG, "Multiple observers registered but only one will be notified of changes.")
+        }
         // Observe the internal MutableLiveData
-        liveDataToObserve.observe(owner, Observer { t ->
-            if (pendingMap[observer.hashCode()] == true) { // don't trigger if the observer wasn't registered
+        super.observe(owner, Observer { t ->
+            if (pending.compareAndSet(true, false)) {
                 observer.onChanged(t)
-                pendingMap[observer.hashCode()] = false
             }
         })
     }
-
+    @MainThread
     override fun setValue(t: T?) {
-        pendingMap.forEach { pendingMap[it.key] = true }
+        pending.set(true)
         super.setValue(t)
+    }
+    /**
+     * Used for cases where T is Void, to make calls cleaner.
+     */
+    @MainThread
+    fun call() {
+        value = null
+    }
+    companion object {
+        private val TAG = "SingleLiveEvent"
     }
 }
